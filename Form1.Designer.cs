cfed0e0128c79ff356b540a23dc78747688c3293
murad <441-05@mail.ru>
2020-06-23 17:34:57 +0300

    minor update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,83 +32,140 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // logBox
             // 
-            this.logBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.logBox.Location = new System.Drawing.Point(0, 305);
+            this.logBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logBox.Location = new System.Drawing.Point(3, 63);
             this.logBox.Name = "logBox";
-            this.logBox.Size = new System.Drawing.Size(800, 145);
+            this.logBox.Size = new System.Drawing.Size(474, 203);
             this.logBox.TabIndex = 0;
             this.logBox.Text = "";
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::DevicesSpecification.Properties.Resources.loading;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(82, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(111, 305);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // listBox1
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(3, 16);
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(105, 134);
+            this.listBox1.Size = new System.Drawing.Size(93, 263);
             this.listBox1.TabIndex = 2;
             // 
             // listBox2
             // 
-            this.listBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(3, 155);
+            this.listBox2.IntegralHeight = false;
+            this.listBox2.Location = new System.Drawing.Point(102, 3);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(105, 147);
+            this.listBox2.Size = new System.Drawing.Size(93, 263);
             this.listBox2.TabIndex = 3;
             // 
-            // groupBox1
+            // start
             // 
-            this.groupBox1.Controls.Add(this.pictureBox1);
-            this.groupBox1.Controls.Add(this.listBox2);
-            this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.groupBox1.Location = new System.Drawing.Point(689, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(111, 305);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
+            this.start.Location = new System.Drawing.Point(3, 3);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 5;
+            this.start.Text = "Запустить";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 39);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.61404F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.38597F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.63063F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(684, 275);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.logBox, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.start, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 133F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(480, 269);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.listBox1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.listBox2, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(483, 3);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(198, 269);
+            this.tableLayoutPanel3.TabIndex = 5;
             // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.logBox);
+            this.ClientSize = new System.Drawing.Size(684, 275);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -119,8 +176,11 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.IO;
-
-using System.Drawing.Imaging;
 
 namespace DevicesSpecification
 {
@@ -20,19 +14,21 @@ namespace DevicesSpecification
 
         public struct ShB_elem
         {
-            public ShB_elem(string aNumber, string aName, string aColC, string aColF, int aCount)
+            public ShB_elem(string aNumber, string aName, string aColC, string aColF, int aCount, string aColI)
             {
                 Number = aNumber;
                 Name = aName;
                 ColC = aColC;
                 ColF = aColF;
                 Count = aCount;
+                ColI = aColI;
             }
-            public string Number { get; }
-            public string Name {get; }
-            public string ColC { get; }
+            public string Number { get; set; }
+            public string Name { get; }
+            public string ColC { get; set; }
             public string ColF { get; }
-            public int Count { get; }
+            public string ColI { get; }
+            public int Count { get; set; }
         };
         public Dictionary<string, List<ShB_elem>> ListShB1;// = new Dictionary<string, ShB1_elem>();
         public Dictionary<string, List<ShB_elem>> ListShB2;// = new Dictionary<string, ShB1_elem>();
@@ -47,27 +43,32 @@ namespace DevicesSpecification
             public string Name { get; }
             public int Count { get; }
         };
-        public Dictionary<string, Dictionary <string, List<RP_elem>>> USPD;
+        public Dictionary<string, Dictionary<string, List<RP_elem>>> USPD;
+        public Dictionary<string, Dictionary<string, List<RP_elem>>> PU;
         public Dictionary<string, Dictionary<string, Dictionary<string, List<RP_elem>>>> TT;
 
         public Form1()
         {
             InitializeComponent();
-            this.Shown += new System.EventHandler(this.Form1_Shown);            
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            //pictureBox1.Dock = DockStyle.Fill;
         }
         private async void Form1_Shown(object sender, EventArgs e)
         {
             ListShB1 = new Dictionary<string, List<ShB_elem>>();
             ListShB2 = new Dictionary<string, List<ShB_elem>>();
             USPD = new Dictionary<string, Dictionary<string, List<RP_elem>>>();
-            TT   = new Dictionary<string, Dictionary<string, Dictionary<string, List<RP_elem>>>>();
-            //await Task.Run(()=>LoadSettings(Directory.GetCurrentDirectory() + "\\Варианты устройства ТТ, Отвл.xlsx"));
+            PU = new Dictionary<string, Dictionary<string, List<RP_elem>>>();
+            TT = new Dictionary<string, Dictionary<string, Dictionary<string, List<RP_elem>>>>();
+            await Task.Run(() => LoadSettings(Directory.GetCurrentDirectory() + "\\Варианты устройства ТТ, Отвл.xlsx"));
             listBox1.Items.AddRange(ListShB1.Keys.ToArray());
             listBox2.Items.AddRange(ListShB2.Keys.ToArray());
             isLoading(false);
         }
 
-        public void  LoadSettings(string file)
+        public void LoadSettings(string file)
         {
             try
             {
@@ -79,7 +80,7 @@ namespace DevicesSpecification
                 Excel.Workbook xlWB;
                 Excel.Worksheet xlSht;
                 xlWB = xlApp.Workbooks.Open(file);
-                
+
                 xlSht = (Excel.Worksheet)xlWB.Worksheets[1];
                 Excel.Range last = xlSht.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
                 var arrData = (object[,])xlSht.get_Range("A1", last).Value;
@@ -98,7 +99,7 @@ namespace DevicesSpecification
                 List<ShB_elem> aList = new List<ShB_elem>();
                 string variatName = "";
                 for (int i = 1; i <= rowCount; i++)
-                {                    
+                {
                     string aName = getStringFromXML(arrData[i, 2]);
                     if (aName != "")
                     {
@@ -115,8 +116,9 @@ namespace DevicesSpecification
                                 aName,
                                 getStringFromXML(arrData[i, 3]),
                                 getStringFromXML(arrData[i, 6]),
-                                getIntFromXML(arrData[i, 7])));
-                    }                    
+                                getIntFromXML(arrData[i, 7]),
+                                getStringFromXML(arrData[i, 9])));
+                    }
                 }
                 ListShB1.Add(variatName, aList);
 
@@ -144,7 +146,8 @@ namespace DevicesSpecification
                                 aName,
                                 getStringFromXML(arrData2[i, 3]),
                                 getStringFromXML(arrData2[i, 6]),
-                                getIntFromXML(arrData2[i, 7])));
+                                getIntFromXML(arrData2[i, 7]),
+                                getStringFromXML(arrData[i, 9])));
                     }
                 }
                 ListShB2.Add(variatName, aList);
@@ -187,7 +190,7 @@ namespace DevicesSpecification
             logBox.AppendText(curentTime + ": " + text + Environment.NewLine, aColor);
         }
 
-        public void isLoading (bool value)
+        public void isLoading(bool value)
         {
             if (InvokeRequired)
             {
@@ -195,29 +198,133 @@ namespace DevicesSpecification
                 return;
             }
             pictureBox1.Visible = value;
+            start.Enabled = !value;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            loadFilters();
+            isLoading(true);
+            loging(0, "Начало");
+            try
+            {
+                await Task.Run(() => loadFilters());
+                await Task.Run(() => GenerateData());
+            }
+            catch (Exception ex)
+            {
+                loging(2, ex.Message);
+            }
+            finally
+            {
+                isLoading(false);
+            }
+        }
+
+        public void GenerateData()
+        {
+            
+            loging(0, "Формирование выходных данных");
+            List<ShB_elem> result = new List<ShB_elem>();
+            foreach (string city in PU.Keys)
+            {
+                foreach (string fider in PU[city].Keys)
+                {
+                    result.Add(new ShB_elem("", city + " " + fider, "", "", 0, ""));
+                    foreach (RP_elem RP in PU[city][fider])
+                    {
+                        string varName = RP.Name;
+                        int varCount = RP.Count;
+                        result.Add(new ShB_elem("", varName, "", "", 0, ""));
+                        try
+                        {
+                            foreach (ShB_elem el in ListShB2[varName.Replace("№", "")])
+                            {
+                                ShB_elem newEl = el;
+                                newEl.Count = newEl.Count * varCount;
+                                result.Add(newEl);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception("Не найден вариант " + varName.Replace("№", "") + "в вариантах устройства ТТ." + ex.Message);
+                        }
+                    }
+                    if (TT.ContainsKey(city) & TT[city].ContainsKey(fider))
+                        foreach (string varName2 in TT[city][fider].Keys)
+                        {
+                            int varCount2 = 0;
+                            foreach (RP_elem RP in TT[city][fider][varName2])                            
+                                varCount2 = varCount2 + RP.Count;
+                            
+                            foreach (ShB_elem el in ListShB1[varName2])
+                            {
+                                ShB_elem newEl = el;
+                                newEl.Count = newEl.Count * varCount2;
+                                result.Add(newEl);
+                            }
+                            result.RemoveAt(result.Count - 1);
+                            int index = 0;
+                            foreach (RP_elem RP in TT[city][fider][varName2])
+                            {
+                                string rpName = RP.Name.Replace("/5", "").Replace("А","");
+                                ShB_elem TP = ListShB1[varName2].Last();
+                                TP.ColC = "ТОП-0,66 У3 "+ rpName +"/ 5 0,5S";
+                                TP.Number = (Int32.Parse(TP.Number) + index).ToString();
+                                result.Add(TP);
+                            }
+                        }
+                }
+            }
+
+            loging(0, "Формирование выходных данных успешно завершено");
+
+            object[,] arr = new object[result.Count, 9];
+
+            int i = -1;
+            foreach (ShB_elem el in result)
+            {
+                i++;
+                arr[i, 0] = el.Number;
+                arr[i, 1] = el.Name;
+                arr[i, 2] = el.ColC;
+                //arr[i, 3] = el.Number;
+                //arr[i, 4] = el.Number;
+                arr[i, 5] = el.ColF;
+                arr[i, 6] = el.Count;
+                //arr[i, 7] = el.Number;
+                arr[i, 8] = el.ColI;
+            }
+            
         }
 
         public void loadFilters()
         {
+            TT.Clear();
+            USPD.Clear();
+            PU.Clear();
+            loging(0, "Чтение файла");
             Excel.Application xlApp = new Excel.Application();
             Excel.Workbook xlWB;
             Excel.Worksheet xlSht;
-            xlWB = xlApp.Workbooks.Open("D:\\work\\VisualStudio\\DevicesSpecification\\Дахадаевские РЭС Реестр потребителей1.xlsx");
+            Excel.Range last;
+            string fileName = textBox1.Text;
+            if (!File.Exists(fileName)) { loging(2, "Проверьте пусть к файлу."); return; }
+            xlWB = xlApp.Workbooks.Open(fileName);
             //Excel.SlicerCache asd = xlWB.SlicerCaches.Item[3];
             //Object[] myObjArray = new Object[1] {"[Кол-во неопрашиваемых ПУ].[Опорная ПС].&[ПО Уркарах Новая]"};
             //asd.VisibleSlicerItemsList = myObjArray;
+
             xlSht = (Excel.Worksheet)xlWB.Worksheets[9];
-            Excel.Range last = xlSht.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            last = xlSht.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
             var arrData = (object[,])xlSht.get_Range("A1", last).Value;
 
             xlSht = (Excel.Worksheet)xlWB.Worksheets[10];
             last = xlSht.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
             var arrData2 = (object[,])xlSht.get_Range("A1", last).Value;
+
+            xlSht = (Excel.Worksheet)xlWB.Worksheets[6];
+            last = xlSht.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            var arrData3 = (object[,])xlSht.get_Range("A1", last).Value;
 
             xlWB.Close(false);
             xlApp.Quit();
@@ -225,7 +332,7 @@ namespace DevicesSpecification
             int rowCount = arrData.GetUpperBound(0);
             int colCount = arrData.GetUpperBound(1);
             //if (colCount < 7) throw new Exception("Не верные вхрдные данные ШБ.");
-            
+
             List<string> ObjName = new List<string>();
             for (int i = 2; i <= colCount; i++)
             {
@@ -252,11 +359,11 @@ namespace DevicesSpecification
                     else
                     {
                         aList = new List<RP_elem>();
-                        for (int j = 2; j <= ObjName.Count+1; j++)
+                        for (int j = 2; j <= ObjName.Count + 1; j++)
                         {
                             int aCount = getIntFromXML(arrData[i, j]);
                             if (aCount > 0)
-                                aList.Add(new RP_elem(ObjName[j-2], aCount));
+                                aList.Add(new RP_elem(ObjName[j - 2], aCount));
                         }
                         city_USPD.Add(aName, aList);
                     }
@@ -295,19 +402,20 @@ namespace DevicesSpecification
                             fider_USPD = new Dictionary<string, List<RP_elem>>();
                             TT.Add(cityName, city_USPD_TT);
                         }
-                            
+
                         cityName = aName;
                         city_USPD_TT = new Dictionary<string, Dictionary<string, List<RP_elem>>>();
                     }
                     else if (aName.Contains("Фидер") || aName.Contains("того"))
-                {
-                    if (aName.Contains("того")) aName = "Фидер №1";
-                    if (fider_USPD.Count > 0)
-                        city_USPD_TT.Add(fideName, fider_USPD);
-                    fideName = aName;
-                    fider_USPD = new Dictionary<string, List<RP_elem>>();
-                }
-                else {
+                    {
+                        if (aName.Contains("того")) aName = "Фидер №1";
+                        if (fider_USPD.Count > 0)
+                            city_USPD_TT.Add(fideName, fider_USPD);
+                        fideName = aName;
+                        fider_USPD = new Dictionary<string, List<RP_elem>>();
+                    }
+                    else
+                    {
                         aList = new List<RP_elem>();
                         for (int j = 2; j <= ObjName.Count + 1; j++)
                         {
@@ -322,6 +430,66 @@ namespace DevicesSpecification
             city_USPD_TT.Add(fideName, fider_USPD);
             TT.Add(cityName, city_USPD_TT);
 
+            rowCount = arrData3.GetUpperBound(0);
+            colCount = arrData3.GetUpperBound(1);
+
+            ObjName = new List<string>();
+            for (int i = 2; i <= colCount; i++)
+            {
+                string jbvNameElem = getStringFromXML(arrData3[3, i]);
+                if (jbvNameElem.Contains("№"))
+                    ObjName.Add(jbvNameElem);
+            }
+            cityName = "";
+            Dictionary<string, List<RP_elem>> fiderList = new Dictionary<string, List<RP_elem>>();
+            for (int i = 4; i <= rowCount; i++)
+            {
+                string aName = getStringFromXML(arrData3[i, 1]);
+                if (aName.Contains("№"))
+                {
+                    for (int j = 2; j <= ObjName.Count + 1; j++)
+                    {
+                        int aCount = getIntFromXML(arrData3[i, j]);
+                        if (aCount > 0)
+                        {
+                            if (!fiderList.ContainsKey(ObjName[j - 2])) fiderList.Add(ObjName[j - 2], new List<RP_elem>());
+                            fiderList[ObjName[j - 2]].Add(new RP_elem(aName, aCount));
+                        }
+                    }
+                }
+                else if (aName.Contains("ПС"))
+                {
+                    if (i > 4)
+                        PU.Add(cityName, fiderList);
+                    cityName = aName;
+                    fiderList = new Dictionary<string, List<RP_elem>>();
+                }
+            }
+            PU.Add(cityName, fiderList);
+
+            loging(0, "Чтение файла завершено успешно");
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (file.Contains(".xls") | file.Contains(".xlsx"))
+                {
+                    textBox1.Text = file;
+                    loging(0, "Добавлен файл " + file);
+                }
+                else
+                {
+                    loging(2, "Не корректный тип файла " + file);
+                }
+            }
         }
     }
     public static class RichTextBoxExtensions
